d2170568e905d3ea0d82dc1fd21f45928ccec780
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2016-04-06 16:51:28 +0200

    Introducing a somehow automated python binding generator

---
+++ cmake/utils.hpp
@@ -0,0 +1,159 @@
+/*
+This file has been taken from the OpenCV project at https:github.com/Itseez/opencv
+It has some small modifications to make it more reusable for generating binding 
+on other projects.
+For your information, the original License is given below:
+
+By downloading, copying, installing or using the software you agree to this license.
+If you do not agree to this license, do not download, install,
+copy or use the software.
+
+
+                          License Agreement
+               For Open Source Computer Vision Library
+                       (3-clause BSD License)
+
+Copyright (C) 2000-2016, Intel Corporation, all rights reserved.
+Copyright (C) 2009-2011, Willow Garage Inc., all rights reserved.
+Copyright (C) 2009-2016, NVIDIA Corporation, all rights reserved.
+Copyright (C) 2010-2013, Advanced Micro Devices, Inc., all rights reserved.
+Copyright (C) 2015-2016, OpenCV Foundation, all rights reserved.
+Copyright (C) 2015-2016, Itseez Inc., all rights reserved.
+Third party copyrights are property of their respective owners.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the names of the copyright holders nor the names of the contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are disclaimed.
+In no event shall copyright holders or contributors be liable for any direct,
+indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused
+and on any theory of liability, whether in contract, strict liability,
+or tort (including negligence or otherwise) arising in any way out of
+the use of this software, even if advised of the possibility of such damage.
+*/
+
+
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
+
+#include "opencv2/core/types_c.h"
+
+
+using namespace cv;
+
+
+typedef std::vector<uchar> vector_uchar;
+typedef std::vector<char> vector_char;
+typedef std::vector<int> vector_int;
+typedef std::vector<float> vector_float;
+typedef std::vector<double> vector_double;
+typedef std::vector<Point> vector_Point;
+typedef std::vector<Point2f> vector_Point2f;
+typedef std::vector<Point3f> vector_Point3f;
+typedef std::vector<Vec2f> vector_Vec2f;
+typedef std::vector<Vec3f> vector_Vec3f;
+typedef std::vector<Vec4f> vector_Vec4f;
+typedef std::vector<Vec6f> vector_Vec6f;
+typedef std::vector<Vec4i> vector_Vec4i;
+typedef std::vector<Rect> vector_Rect;
+typedef std::vector<Rect2d> vector_Rect2d;
+typedef std::vector<KeyPoint> vector_KeyPoint;
+typedef std::vector<Mat> vector_Mat;
+typedef std::vector<DMatch> vector_DMatch;
+typedef std::vector<String> vector_String;
+typedef std::vector<Scalar> vector_Scalar;
+
+typedef std::vector<std::vector<char> > vector_vector_char;
+typedef std::vector<std::vector<Point> > vector_vector_Point;
+typedef std::vector<std::vector<Point2f> > vector_vector_Point2f;
+typedef std::vector<std::vector<Point3f> > vector_vector_Point3f;
+typedef std::vector<std::vector<DMatch> > vector_vector_DMatch;
+
+
+#ifdef HAVE_OPENCV_FEATURES2D
+typedef SimpleBlobDetector::Params SimpleBlobDetector_Params;
+#endif
+
+#ifdef HAVE_OPENCV_FLANN
+typedef cvflann::flann_distance_t cvflann_flann_distance_t;
+typedef cvflann::flann_algorithm_t cvflann_flann_algorithm_t;
+#endif
+
+#ifdef HAVE_OPENCV_STITCHING
+#include "opencv2/stitching.hpp"
+typedef Stitcher::Status Status;
+#endif
+
+
+static PyObject* opencv_error = 0;
+
+
+static int failmsg(const char *fmt, ...)
+{
+    char str[1000];
+
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(str, sizeof(str), fmt, ap);
+    va_end(ap);
+
+    PyErr_SetString(PyExc_TypeError, str);
+    return 0;
+}
+
+
+class PyAllowThreads
+{
+public:
+    PyAllowThreads() : _state(PyEval_SaveThread()) {}
+    ~PyAllowThreads()
+    {
+        PyEval_RestoreThread(_state);
+    }
+private:
+    PyThreadState* _state;
+};
+
+class PyEnsureGIL
+{
+public:
+    PyEnsureGIL() : _state(PyGILState_Ensure()) {}
+    ~PyEnsureGIL()
+    {
+        PyGILState_Release(_state);
+    }
+private:
+    PyGILState_STATE _state;
+};
+
+
+#define ERRWRAP2(expr) \
+try \
+{ \
+    PyAllowThreads allowThreads; \
+    expr; \
+} \
+catch (const cv::Exception &e) \
+{ \
+    PyErr_SetString(opencv_error, e.what()); \
+    return 0; \
+}
+
+
+#endif // __UTILS_HPP__
